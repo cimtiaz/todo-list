7ef515748d141aa08129e9cdf3477b9030c1f3b7
Imtiaz <7255604+cimtiaz@users.noreply.github.com>
2019-10-31 14:01:20 +0100

    updated all comments for Main class

--- src/main/java/org/sda/todolist/Main.java
+++ src/main/java/org/sda/todolist/Main.java
@@ -12,15 +12,25 @@ import java.util.Scanner;
 
 
 public class Main {
+    // A string to hold the data file name which contains all tasks and their details
     public static String filename = "tasks.obj";
 
+    /**
+     * main method to run the command line based "To Do List" application
+     * @param args array of String holding command line parameters
+     */
     public static void main(String args[]) {
+        // An object of TodoList to hold all tasks and their data
         TodoList todoList = new TodoList();
+
+        //A string to hold the choice that will be entered by the user
         String menuChoice = "-17";
 
         try {
             Scanner input = new Scanner(System.in);
 
+            // reading the date from task data file
+            // if this is the first time, a message will be shown that no data file is found
             todoList.readFromFile(filename);
 
             Messages.showMessage("Welcome to ToDoList", false);
@@ -50,12 +60,14 @@ public class Main {
                 }
             }
 
+            // saving the task details in data file
+            // if this is the first time, a new task file will be created
             todoList.saveToFile(filename);
             Messages.byeMessage();
 
         } catch (Exception e) {
             Messages.showMessage("UNCAUGHT EXCEPTION THROWN", true);
-            System.out.println("Trying to save the unsaved tasks in data file");
+            System.out.println("Trying to write the unsaved data of all tasks in data file");
             todoList.saveToFile(filename);
             System.out.println(e.getMessage());
             System.out.println(e.getStackTrace());
