d55cb45ae1ecd92874540300701df21e1bf570e9
Imtiaz <7255604+cimtiaz@users.noreply.github.com>
2019-10-31 13:01:39 +0100

    Added comments to all tests

--- src/test/java/org/sda/todolist/TaskTest.java
+++ src/test/java/org/sda/todolist/TaskTest.java
@@ -10,6 +10,15 @@ import java.time.format.DateTimeParseException;
 
 import static org.junit.Assert.*;
 
+/**
+ * This class is created for unit testing for model class, i.e., Task (Task.java)
+ *
+ * @author  Imtiaz
+ * @version 1.0
+ * @since   2019-10-11
+ **/
+
+
 public class TaskTest {
     Task task;
 
@@ -18,16 +27,34 @@ public class TaskTest {
     LocalDate validDueDate=LocalDate.parse("2020-12-31");
 
 
+    /**
+     * This method will execute before executing any Test.
+     * This method will initialize the task object with vaid test parameters.
+     * @throws Exception
+     */
     @Before
     public void setUp() throws Exception {
-        task = new Task(validTitle,validProject,validDueDate);
+        try {
+            task = new Task(validTitle, validProject, validDueDate);
+        } catch (Exception e) {
+            System.out.println("There are few errors in the testing of Task class");
+            System.out.println("Following message could be helpful to identify the cause:");
+            System.out.println(e.getMessage());
+        }
     }
 
+    /**
+     * This method will validate the task.getTitle() method for valid parameter
+     */
     @Test
     public void testValidTitle() {
         assertEquals(validTitle,task.getTitle());
     }
 
+    /**
+     * This method will validate the task.setTitle() method for an empty string "" as invalid parameter
+     * which should result as NullPointerException
+     */
     @Test
     public void testEmptyTitle() {
         boolean success=false;
@@ -42,6 +69,10 @@ public class TaskTest {
         assertEquals(true,success);
     }
 
+    /**
+     * This method will validate the task.setTitle() method for trimming an empty string "     " as invalid parameter
+     * which should result as NullPointerException
+     */
     @Test
     public void testTrimEmptyTitle() {
         boolean success=false;
@@ -56,6 +87,10 @@ public class TaskTest {
         assertEquals(true,success);
     }
 
+    /**
+     * This method will validate the task.setTitle() method for null as invalid parameter
+     * which should result as NullPointerException
+     */
     @Test
     public void testNullTitle() {
         boolean success=false;
@@ -70,22 +105,37 @@ public class TaskTest {
         assertEquals(true,success);
     }
 
+    /**
+     * This method will validate the task.getProject() method for valid parameter
+     * Project title is allowed be empty
+     */
     @Test
     public void testValidProject() {
         assertEquals(validProject,task.getProject());
     }
 
+    /**
+     * This method will validate the task.setProject() method for empty string "" as parameter
+     * And it will also test the task.getProject
+     * Project title is allowed be empty
+     */
     @Test
     public void testEmptyProject() {
         task.setProject("");
         assertEquals("",task.getProject());
     }
 
+    /**
+     * This method will validate the task.getDueDate() method
+     */
     @Test
     public void testvalidDueDate() {
         assertEquals(validDueDate,task.getDueDate());
     }
 
+    /**
+     * This method will validate the task.setDueDate() method with a wrong format date as parameter
+     */
     @Test
     public void testIncorrectFormatDueDate() {
         boolean success=false;
@@ -100,6 +150,10 @@ public class TaskTest {
         assertEquals(true,success);
     }
 
+    /**
+     * This method will validate the task.setDueDate() method with a past due date
+     * If a past due date is tried to set, it should throw DateTimeException
+     */
     @Test
     public void testPastDueDate() {
         boolean success=false;
